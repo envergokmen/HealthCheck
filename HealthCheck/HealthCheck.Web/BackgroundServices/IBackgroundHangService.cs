8dbaefbff75ed395bdc29d80572b9a74dc974795
Enver GÖKMEN <envergokmen@gmail.com>
2020-09-24 22:29:14 +0300

    cc

--- HealthCheck/HealthCheck.Web/BackgroundServices/IBackgroundHangService.cs
+++
@@ -1,9 +0,0 @@
-﻿using HealthCheck.Models.DTOs.TargetApps;
-
-namespace HealthCheck.Web
-{
-    public interface IBackgroundHangService
-    {
-        void CheckDownOrAlive(TargetAppDto item);
-    }
-}
